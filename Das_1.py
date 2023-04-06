9f384ab06e6a6fd12590c9df6a95cbdfa8b032f2
Jon83Carvalho <jonas.carvalho@gmail.com>
2023-04-06 11:03:21 -0300

    as a program

--- Das_1.py
+++ Das_1.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+
+def main ():
+    x = st.slider("Select a value")
+    st.write(x, "squared is", x * x)
+
+
+if __name__== "__main__":
+
+    main()
