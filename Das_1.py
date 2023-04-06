17ff13706c3d1f01d485a41fa4872abbc714eeab
Jon83Carvalho <jonas.carvalho@gmail.com>
2023-04-06 11:43:30 -0300

    FIRST APP

--- Das_1.py
+++ Das_1.py
@@ -7,10 +7,12 @@ def main ():
     x = st.slider("Select a value")
     st.write(x, "squared is", x * x)
 
-    st.write(pd.DataFrame({
+    df=pd.DataFrame({
                         'first column': [1, 2, 3, 4],
                         'second column': [10, 20, 30, 40]
-                    }))
+                    })
+
+    st.write(df)
 
     
     st.write("I LIKE THIS")
@@ -20,6 +22,35 @@ def main ():
                         columns=('col %d' % i for i in range(20)))
 
     st.dataframe(dataframe.style.highlight_max(axis=0))
+
+    if st.checkbox('Show dataframe'):
+        chart_data = pd.DataFrame(
+            np.random.randn(20, 3),
+            columns=['a', 'b', 'c'])
+
+        chart_data
+
+    add_selectbox = st.sidebar.selectbox(
+            'How would you like to be contacted?',
+            ('Email', 'Home phone', 'Mobile phone')
+        )
+
+# Add a slider to the sidebar:
+    add_slider = st.sidebar.slider(
+        'Select a range of values',
+        0.0, 100.0, (25.0, 75.0)
+    )
+
+    left_column, right_column = st.columns(2)
+    # You can use a column just like st.sidebar:
+    left_column.button('Press me!')
+
+    with right_column:
+        chosen = st.radio(
+            'Sorting hat',
+            ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+        st.write(f"You are in {chosen} house!")
+    
 
 
 
