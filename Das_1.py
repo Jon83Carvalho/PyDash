ba6e7998a624364dda675a70a500cbd60f01b6db
Jon83Carvalho <jonas.carvalho@gmail.com>
2023-04-06 11:18:10 -0300

    update

--- Das_1.py
+++ Das_1.py
@@ -1,9 +1,26 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 
 def main ():
     x = st.slider("Select a value")
     st.write(x, "squared is", x * x)
+
+    st.write(pd.DataFrame({
+                        'first column': [1, 2, 3, 4],
+                        'second column': [10, 20, 30, 40]
+                    }))
+
+    
+    st.write("I LIKE THIS")
+
+    dataframe = pd.DataFrame(
+                        np.random.randn(10, 20),
+                        columns=('col %d' % i for i in range(20)))
+
+    st.dataframe(dataframe.style.highlight_max(axis=0))
+
 
 
 if __name__== "__main__":
